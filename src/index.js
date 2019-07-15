ed9fdea568093bc12af67c0a428d312472ec1c7b
sobucki <sobucki@gmail.com>
2019-07-15 17:43:58 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import '~/config/ReactotronConfig';
+import '~/config/DevToolsConfig';
+import '~/config/StatusBarConfig';
+
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import Routes from '~/routes';
+import store from '~/store';
+
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+);
+
+export default App;
